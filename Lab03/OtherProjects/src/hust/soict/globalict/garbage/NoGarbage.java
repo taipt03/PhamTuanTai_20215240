c205118fba5c436d6a7260a84ef0bb34eaa18cec
Tai Pham <tuantai0812@gmail.com>
2023-05-29 21:06:50 +0700

    lab04

--- Lab03/OtherProjects/src/hust/soict/globalict/garbage/NoGarbage.java
+++
@@ -1,9 +0,0 @@
-package hust.soict.globalict.garbage;
-
-public class NoGarbage {
-	public void cleanUpGarbage() {
-		System.gc();
-		System.runFinalization();
-		System.out.println("Garbage cleared\n");
-	}
-}
